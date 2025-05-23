cbc3cc2c662a8ed1be9cdd366edabf04422c5dec
Olanrewaju <olaifalanrewaju01@gmail.com>
2025-05-23 01:12:35 +0100

    work

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors. It's strongly recommended to fix
+    // ESLint issues to maintain code quality.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
